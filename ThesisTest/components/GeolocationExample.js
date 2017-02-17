ffbfbc3356e42ef6945644631d578c4ed9b14ea5
Nick Cobbett <nicholas.cobbett@gmail.com>
2017-02-16 21:01:20 -0800

    gps testing

--- ThesisTest/components/GeolocationExample.js
+++ ThesisTest/components/GeolocationExample.js
@@ -1,93 +1,186 @@
 import React, { Component } from 'react';
 import {AppRegistry, StyleSheet, Text, View, ListView} from 'react-native';
-// import Accelerometer from 'NativeModules';
+import BackgroundGeolocation from 'react-native-background-geolocation';
 
-var {
-  DeviceEventEmitter // will emit events that you can listen to
-} = React;
+// var {
+//   DeviceEventEmitter // will emit events that you can listen to
+// } = React;
+
 
 class GeolocationExample extends React.Component {
-  constructor(props) {
-    super(props);
-    this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    this.state = {
-      dataSource: this.ds.cloneWithRows(['hello'])
-    };
+
+
+componentWillMount() {
+
+    // This handler fires whenever bgGeo receives a location update.
+    BackgroundGeolocation.on('location', this.onLocation);
+
+    // This handler fires when movement states changes (stationary->moving; moving->stationary)
+    BackgroundGeolocation.on('motionchange', this.onMotionChange);
+    // this.setState()
+    // Now configure the plugin.
+    BackgroundGeolocation.configure({
+      // Geolocation Config
+      desiredAccuracy: 0,
+      stationaryRadius: 1,
+      distanceFilter: 1,
+      // Activity Recognition
+      stopTimeout: 1,
+      // Application config
+      debug: true, // <-- enable for debug sounds & notifications
+      logLevel: BackgroundGeolocation.LOG_LEVEL_VERBOSE,
+      stopOnTerminate: false,   // <-- Allow the background-service to continue tracking when user closes the app.
+      startOnBoot: true,        // <-- Auto start tracking when device is powered-up.
+      // HTTP / SQLite config
+      url: 'https://requestb.in/124tk7t1',
+      autoSync: true,         // <-- POST each location immediately to server
+      params: {               // <-- Optional HTTP params
+        "auth_token": "maybe_your_server_authenticates_via_token_YES?"
+      }
+    }, function(state) {
+      console.log("- BackgroundGeolocation is configured and ready: ", state.enabled);
+
+      if (!state.enabled) {
+        BackgroundGeolocation.start(function() {
+          console.log("- Start success");
+        });
+      }
+    });
   }
 
-  watchID: ?number = null;
-
-  sendData(coordinates) {
-    fetch('https://requestb.in/124tk7t1', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(coordinates)
-    }).then(success => {
-      console.log('success###')
-    }).catch(error => {
-      console.log('error###', error)
-    })
-  }
-
-  getLatAndLong(position) {
-    var coords = JSON.parse(position).coords;
-    var lat = coords.latitude;
-    var long = coords.longitude;
-
-    this.setState({
-      dataSource: this.ds.cloneWithRows({lat: lat, long: long})
-    })
-
-    console.log(lat, long)
-  }
-
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition(position => {
-        var initialPosition = JSON.stringify(position);
-        this.getLatAndLong(initialPosition);
-      }, error => alert(JSON.stringify(error)),
-      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
-    );
-
-    this.watchID = navigator.geolocation.watchPosition(position => {
-      var lastPosition = JSON.stringify(position);
-      this.sendData(lastPosition);
-      this.getLatAndLong(lastPosition);
-    }, error => {
-      console.log('error')
-    }, {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 1});
-
-    console.log('#####', this.watchID)
-
-  }
-
+  // You must remove listeners when your component unmounts
   componentWillUnmount() {
-    navigator.geolocation.clearWatch(this.watchID);
+    // Remove BackgroundGeolocation listeners
+    BackgroundGeolocation.un('location', this.onLocation);
+    BackgroundGeolocation.un('motionchange', this.onMotionChange);
   }
+  onLocation(location) {
+    // fetch('https://requestb.in/124tk7t1', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify(location)
+    // }).then(success => {
+    //   console.log('success###')
+    // }).catch(error => {
+    //   console.log('error###', error)
+    // })
+    console.log('- [js]location: ', JSON.stringify(location));
+  }
+  onMotionChange(location) {
+    console.log('- [js]motionchanged: ', JSON.stringify(location));
+  }
+
 
   render() {
     return (
-      <View style={{flex: 1, paddingTop: 22}}>
-        <ListView
-          dataSource={this.state.dataSource}
-          renderRow={(rowData) => <Text>{rowData}</Text>}
-        />
-      </View>
+        <Text>Hello</Text>
     );
   }
 }
 
-var styles = StyleSheet.create({
-  view: {
-    marginTop: 20
-  },
-  title: {
-    fontWeight: '500',
-  },
-});
+
+// class GeolocationExample extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+//     this.state = {
+//       dataSource: this.ds.cloneWithRows(['hello'])
+//     };
+//   }
+
+//   watchID: ?number = null;
+
+//   sendData(coordinates) {
+//     fetch('https://requestb.in/124tk7t1', {
+//       method: 'POST',
+//       headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(coordinates)
+//     }).then(success => {
+//       console.log('success###')
+//     }).catch(error => {
+//       console.log('error###', error)
+//     })
+//   }
+
+//   getLatAndLong(position) {
+//     var coords = JSON.parse(position).coords;
+//     var lat = coords.latitude;
+//     var long = coords.longitude;
+
+//     this.setState({
+//       dataSource: this.ds.cloneWithRows({lat: lat, long: long})
+//     })
+
+//     console.log(lat, long)
+//   }
+
+//   // getLatAndLong(position) {
+//   //   bgGeo.configure({
+//   //     desiredAccuracy: 0,
+//   //     distanceFilter: 50
+//   //   }, function(state) {
+//   //     console.log('- Configure success.  Current state: ', state);
+//   //   });
+//   //   // Use #setConfig if you need to change options after you've executed #configure
+
+//   //   bgGeo.setConfig({
+//   //       desiredAccuracy: 10,
+//   //       distanceFilter: 10
+//   //   }, function(state) {
+//   //       console.log('- setConfig success.  Current state: ', state);
+//   //   });
+//   // }
+
+//   componentDidMount() {
+//     navigator.geolocation.getCurrentPosition(position => {
+//         var initialPosition = JSON.stringify(position);
+//         this.getLatAndLong(initialPosition);
+//       }, error => alert(JSON.stringify(error)),
+//       {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+//     );
+
+//     this.watchID = navigator.geolocation.watchPosition(position => {
+//       var lastPosition = JSON.stringify(position);
+//       this.sendData(lastPosition);
+//       this.getLatAndLong(lastPosition);
+//     }, error => {
+//       console.log('error')
+//     }, {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 1});
+
+//     console.log('#####', this.watchID)
+
+//   }
+
+//   componentWillUnmount() {
+//     navigator.geolocation.clearWatch(this.watchID);
+//   }
+
+//   render() {
+//     return (
+//       <View style={{flex: 1, paddingTop: 22}}>
+//         <ListView
+//           dataSource={this.state.dataSource}
+//           renderRow={(rowData) => <Text>{rowData}</Text>}
+//         />
+//       </View>
+//     );
+//   }
+// }
+
+// var styles = StyleSheet.create({
+//   view: {
+//     marginTop: 20
+//   },
+//   title: {
+//     fontWeight: '500',
+//   },
+// });
 
 export default GeolocationExample
 
--- ThesisTest/index.ios.js
+++ ThesisTest/index.ios.js
@@ -1,43 +1,149 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  * @flow
+//  */
 
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Vibration
 } from 'react-native';
-import GeolocationExample from './components/GeolocationExample.js';
+// import GeolocationExample from './components/GeolocationExample.js';
+// import Foo from './components/foo.js';
 
-export default class ThesisTest extends Component {
+// export default class ThesisTest extends Component {
+//   render() {
+//     return (
+//       <Foo/>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
+
+// import React, { Component } from 'react';
+// import {AppRegistry, StyleSheet, Text, View, ListView} from 'react-native';
+import BackgroundGeolocation from "react-native-background-geolocation";
+
+var ThesisTest = React.createClass({
+
+  componentWillMount() {
+    // var pattern = [0];
+    // var interval = setInterval(() => {
+    //   Vibration.vibrate(pattern);
+    // }, 10000);
+    // This handler fires whenever bgGeo receives a location update.
+    BackgroundGeolocation.on('location', this.onLocation);
+
+    // This handler fires when movement states changes (stationary->moving; moving->stationary)
+    BackgroundGeolocation.on('motionchange', this.onMotionChange);
+
+    BackgroundGeolocation.on('heartbeat', this.onMotionChange);
+    // BackgroundGeolocation.on('heartbeat', this.onMotionChange);
+
+    // Now configure the plugin.
+    BackgroundGeolocation.configure({
+      // Geolocation Config
+      heartbeatInterval: 5,
+      activityRecognitionInterval: 0,
+      disableElasticity: true,
+      desiredAccuracy: 0,
+      stationaryRadius: 1,
+      distanceFilter: 1,
+      // Activity Recognition
+      stopTimeout: 10000,
+      // Application config
+      debug: true, // <-- enable for debug sounds & notifications
+      logLevel: BackgroundGeolocation.LOG_LEVEL_VERBOSE,
+      stopOnTerminate: false,   // <-- Allow the background-service to continue tracking when user closes the app.
+      startOnBoot: true,        // <-- Auto start tracking when device is powered-up.
+      // HTTP / SQLite config
+      url: 'https://requestb.in/124tk7t1',
+      autoSync: true,         // <-- POST each location immediately to server
+      params: {               // <-- Optional HTTP params
+        "auth_token": "maybe_your_server_authenticates_via_token_YES?"
+      }
+    }, function(state) {
+      console.log("- BackgroundGeolocation is configured and ready: ", state.enabled);
+
+      if (!state.enabled) {
+        BackgroundGeolocation.start(function() {
+          console.log("- Start success");
+        });
+      }
+    });
+  },
+  // You must remove listeners when your component unmounts
+  componentWillUnmount() {
+    // Remove BackgroundGeolocation listeners
+    BackgroundGeolocation.un('location', this.onLocation);
+    BackgroundGeolocation.un('motionchange', this.onMotionChange);
+  },
+  onLocation(location) {
+    console.log('- [js]location: ', JSON.stringify(location));
+
+    // fetch('https://requestb.in/124tk7t1', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify(location)
+    //   }).then(success => {
+    //     console.log('success###')
+    //   }).catch(error => {
+    //     console.log('error###', error)
+    //   })
+    // }
+    var pattern = [0];
+    // var patternLiteral = '[]';
+    Vibration.vibrate(pattern);
+  },
+  onMotionChange(location) {
+    console.log('- [js]motionchanged: ', JSON.stringify(location));
+    // fetch('https://requestb.in/124tk7t1', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify(location)
+    //   }).then(success => {
+    //     console.log('success###')
+    //   }).catch(error => {
+    //     console.log('error###', error)
+    //   })
+    // }
+    var pattern = [0];
+    // var patternLiteral = '[]';
+    Vibration.vibrate(pattern);
+
+  },
+
   render() {
-    return (
-      <GeolocationExample/>
-    );
+    return (<Text>hello</Text>)
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
 });
 
 AppRegistry.registerComponent('ThesisTest', () => ThesisTest);
